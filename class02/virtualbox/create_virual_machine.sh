b4102c89bd4297216b50b35544be8ec05d85bfb5
martindutria <111375472+martindutria@users.noreply.github.com>
2022-09-21 01:18:46 -0300

    Update create_virual_machine.sh

--- class02/virtualbox/create_virual_machine.sh
+++ class02/virtualbox/create_virual_machine.sh
@@ -14,13 +14,13 @@ echo -e "\nRAM MEMORY[MB]:"
 read MEMORY
 echo -e "\nCPUs Number:"
 read CPU
+echo -e "\nDISK SIZE[MB]:"
+read DISK
 echo ""
 
-vboxmanage createvm --name $NAME --ostype Ubuntu_64 --register --basefolder /home/franzmejia/Documents/JalaUniversity/class02/virtualbox/ --default
+vboxmanage createvm --name $NAME --ostype Ubuntu_64 --register --basefolder C:\Jala\Devops\vm --default
 vboxmanage modifyvm $NAME --ioapic on
 vboxmanage modifyvm $NAME --memory $MEMORY --cpus $CPU
 vboxmanage modifyvm $NAME --nic1 nat
-
-# To do, create storage and attach it
-
-
+VBoxManage createhd --size $DISK --filename C:\Jala\Devops\vm\test\disks\test_disk
+vboxmanage storageattach $NAME --storagectl "SATA" --device 0 --port 0 --type hdd --medium C:\Jala\Devops\vm\disks\test_disk.vdi
