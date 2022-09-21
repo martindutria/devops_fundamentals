e39c8aeaf595a411f59f833b11ead396016950de
martindutria <111375472+martindutria@users.noreply.github.com>
2022-09-21 01:27:21 -0300

    Update create_virual_machine.sh

--- class02/virtualbox/create_virual_machine.sh
+++ class02/virtualbox/create_virual_machine.sh
@@ -22,5 +22,5 @@ vboxmanage createvm --name $NAME --ostype Ubuntu_64 --register --basefolder C:\J
 vboxmanage modifyvm $NAME --ioapic on
 vboxmanage modifyvm $NAME --memory $MEMORY --cpus $CPU
 vboxmanage modifyvm $NAME --nic1 nat
-VBoxManage createhd --size $DISK --filename C:\Jala\Devops\vm\$NAME\disks\$NAME_disk
-vboxmanage storageattach $NAME --storagectl "SATA" --device 0 --port 0 --type hdd --medium C:\Jala\Devops\vm\disks\$NAME_disk.vdi
+vboxmanage createhd --size $DISK --filename C:\Jala\Devops\vm\$NAME\disks\$NAME_disk
+vboxmanage storageattach $NAME --storagectl "SATA" --device 0 --port 0 --type hdd --medium C:\Jala\Devops\vm\$NAME\disks\$NAME_disk.vdi
